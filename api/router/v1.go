6dac3fd5ffb74ea7bf8e51262a31f2a2b911b966
JAKE <lee@lou2.kr>
2024-06-21 05:51:34 +0000

    mod, endpoint 수정

--- api/router/v1.go
+++ api/router/v1.go
@@ -18,38 +18,38 @@ func V1Router() *http.ServeMux {
 		userMux.HandleFunc("POST /", userHandler.CreateUserHandler)
 
 		// 사용자 확인 및 인증 코드 전송
-		userMux.HandleFunc("POST /verify", userHandler.SendVerifyCodeHandler)
+		userMux.HandleFunc("POST /verify/", userHandler.SendVerifyCodeHandler)
 		// 인증 코드 확인 및 사용자 업데이트
-		userMux.HandleFunc("PATCH /verify/{code}", userHandler.VerifyCodeHandler)
+		userMux.HandleFunc("PATCH /verify/{code}/", userHandler.VerifyCodeHandler)
 
 		// 비밀번호 재설정 코드 전송
-		userMux.HandleFunc("POST /password", userHandler.SendPasswordResetCodeHandler)
+		userMux.HandleFunc("POST /password/", userHandler.SendPasswordResetCodeHandler)
 		// 비밀번호 재설정
-		userMux.HandleFunc("PATCH /password", userHandler.ResetPasswordHandler)
+		userMux.HandleFunc("PATCH /password/", userHandler.ResetPasswordHandler)
 	}
 	authMux := http.NewServeMux()
 	{
 		// 토큰 발급
-		authMux.HandleFunc("POST /token", authHandler.CreateTokenHandler)
+		authMux.HandleFunc("POST /token/", authHandler.CreateTokenHandler)
 		// 토큰 갱신
-		authMux.HandleFunc("POST /token/refresh", authHandler.RefreshTokenHandler)
+		authMux.HandleFunc("POST /token/refresh/", authHandler.RefreshTokenHandler)
 
 		// Client 생성
-		authMux.Handle("POST /client", middleware.AuthMiddleware(middleware.PermissionMiddleware(clientModel.ScopeWriteClient, http.HandlerFunc(authHandler.CreateClientHandler))))
+		authMux.Handle("POST /client/", middleware.AuthMiddleware(middleware.PermissionMiddleware(clientModel.ScopeWriteClient, http.HandlerFunc(authHandler.CreateClientHandler))))
 		// Client 조회
-		authMux.Handle("GET /client", middleware.AuthMiddleware(middleware.PermissionMiddleware(clientModel.ScopeReadClient, http.HandlerFunc(authHandler.GetClientsHandler))))
+		authMux.Handle("GET /client/", middleware.AuthMiddleware(middleware.PermissionMiddleware(clientModel.ScopeReadClient, http.HandlerFunc(authHandler.GetClientsHandler))))
 		// // Client 삭제
-		authMux.Handle("DELETE /client/{id}", middleware.AuthMiddleware(middleware.PermissionMiddleware(clientModel.ScopeWriteClient, http.HandlerFunc(authHandler.DeleteClientHandler))))
+		authMux.Handle("DELETE /client/{id}/", middleware.AuthMiddleware(middleware.PermissionMiddleware(clientModel.ScopeWriteClient, http.HandlerFunc(authHandler.DeleteClientHandler))))
 
 		// 로그인
-		authMux.HandleFunc("POST /login", authHandler.LoginHandler)
+		authMux.HandleFunc("POST /login/", authHandler.LoginHandler)
 		// 로그아웃
-		authMux.HandleFunc("POST /logout", middleware.AuthMiddleware(http.HandlerFunc(authHandler.LogoutHandler)))
+		authMux.HandleFunc("POST /logout/", middleware.AuthMiddleware(http.HandlerFunc(authHandler.LogoutHandler)))
 	}
 	clientMux := http.NewServeMux()
 	{
 		// 내 정보 조회
-		clientMux.HandleFunc("GET /me", middleware.AuthMiddleware(middleware.LimitPerSecondMiddleware(clientModel.ScopeReadMe, http.HandlerFunc(clientHandler.GetMeHandler))))
+		clientMux.HandleFunc("GET /me/", middleware.AuthMiddleware(middleware.LimitPerSecondMiddleware(clientModel.ScopeReadMe, http.HandlerFunc(clientHandler.GetMeHandler))))
 	}
 	mux.Handle("/user/", http.StripPrefix("/user", userMux))
 	mux.Handle("/auth/", http.StripPrefix("/auth", authMux))
